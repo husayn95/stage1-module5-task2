82405fa115207332991832c82df0d052a64513b1
Husayn Ganiev <hg222ii@student.lnu.se>
2023-02-26 17:37:49 +0100

    firstt attempt

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -2,7 +2,13 @@ package com.epam.mjc;
 
 public class InterfaceCreator {
 
-    public Operation<Integer> divideBy(Integer divider) {
-        throw new UnsupportedOperationException("You should implement this method.");
+    public static Operation<Integer> divideBy(Integer divider) {
+       //  Operation divide = (x) -> x.forEach(i-> i);
+      return n -> n.forEach(integer ->  integer/divider);
+        // throw new UnsupportedOperationException("You should implement this method.");
+    }
+
+    public static void main(String[] args) {
+        System.out.println();
     }
 }
