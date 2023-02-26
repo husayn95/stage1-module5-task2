291320550096e598381c211bc72d7e34cc6a4da9
Husayn Ganiev <hg222ii@student.lnu.se>
2023-02-26 18:24:15 +0100

    firstt attempt

--- src/main/java/com/epam/mjc/InterfaceCreator.java
+++ src/main/java/com/epam/mjc/InterfaceCreator.java
@@ -1,14 +1,23 @@
 package com.epam.mjc;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class InterfaceCreator {
 
     public static Operation<Integer> divideBy(Integer divider) {
-       //  Operation divide = (x) -> x.forEach(i-> i);
-      return n -> n.forEach(integer ->  integer/divider);
+       // Operation divide = (x) -> x.forEach(i-> i/divider);
+      return n -> n.stream().map(integer -> integer/divider).collect(Collectors.toList());
         // throw new UnsupportedOperationException("You should implement this method.");
     }
 
     public static void main(String[] args) {
-        System.out.println();
+        List<Integer> integers = new ArrayList<>();
+        integers.add(2);
+        integers.add(4);
+        integers.add(6);
+        System.out.println(divideBy(2).apply(integers));
     }
 }
